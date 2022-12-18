a95a44a8533a58c1c715d1f7d9ade19b06c1f70c
Josh Blood <jblood92@pm.me>
2022-12-17 21:08:29 -0500

    styled components setup, css added and working

--- src/Components/NasaPhoto.js
+++ src/Components/NasaPhoto.js
@@ -1,13 +1,40 @@
 import React from "react";
+import styled, {keyframes} from "styled-components";
+
+
+const kf = keyframes`
+  50% {
+    transform: scale(.8);
+  }
+  100% {
+    transform: scale(1);
+    opacity: 1;
+  }
+`
+
+const PhotoWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    opacity: 0;
+    transform: scale(2);
+    animation: ${kf} .7s ease-in-out forwards;
+`
+const Explanation = styled.p`
+    max-width: 50vw;
+`
+const Image = styled.img`
+    max-width: 50vw;
+`
 
 const NasaPhoto = (props) => {
     return (
-        <div className="nasa-photo-wrapper">
+        <PhotoWrapper>
             <h3>{props.photo.title}</h3>
             <p>{props.photo.date}</p>
-            <img src={props.photo.hdurl} />
-            <p className="explanation">{props.photo.explanation}</p>
-        </div>
+            <Image src={props.photo.hdurl} />
+            <Explanation>{props.photo.explanation}</Explanation>
+        </PhotoWrapper>
 
     ) 
 }
